dad3c3edca64285bf1247eff8bac3614e005c6a0
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-22 11:49:57 -0400

    Added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <div className='container'>
+      <header className="App-header">Hello
+      
+      </header>
         
-     
+    <main>
+        <Dictionary />
+    </main>
 
       
-    </div>
-  );
+   
+
+    <footer className='App-footer'>
+    <small>This project was coded by{" "} <a href="https://responsive-portfolio-marilu.netlify.app/" target="_blank" rel="noreferrer">Marilu Chacon</a> 
+    {" "}  and is {" "} 
+    <a href="https://github.com/lilbxbori/dictionary-project-react" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> 
+    {" "} and {" "}  
+    <a href="https://dictionary-project-react-mchacon.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify </a>
+    </small>
+    </footer>
+      
+  </div>
+  </div>
+  ); 
 }
 
 export default App;
