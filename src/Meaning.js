4086bd00cf09c9d9ad8710c34a7329a5dc2c8930
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-23 19:43:42 -0400

    Created component Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Synonyms from "./Synonyms";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -10,7 +10,7 @@ export default function Meaning(props) {
       <div className="fw-bold">Example</div>
       <p>{props.meaning.example}</p>
       <div className="fw-bold">Synonyms</div>
-      {/* <Synonyms synonyms={props.meaning.synonyms} /> */}
+      <Synonyms synonyms={props.meaning.synonyms} />
       <br />
     </div>
   );
---
+++ src/Synonyms.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  if (props.synonyms) {
+    return (
+      <ul className="synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return (
+           <li key={index}>{synonym}</li>
+            );
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+}
