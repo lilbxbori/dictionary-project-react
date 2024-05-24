06551757e144eac3c56598c57a5f224b2cc18fd9
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-24 00:26:01 -0400

    Adding Photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,28 +1,41 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null); 
+
+function search(event) {
+    setKeyword(event.target.value);
+  }
+
+  function handleDictionaryResponse(response) {
+    setResults(response.data);
+  }
+
+ function handleImagesResponse(response) {
+    // setResults(response.data);
+    console.log(response);
+    setPhotos(response.data.photos);
+  } 
 
   function handleSearch(event) {
     event.preventDefault();
 
     let apiKey = `7a6tfo3aa33dcf22944a8db00a0bf65c`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let imagesApiKey = `487f36oead7740fff43e3a5t4ccd7b4f`;
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
-  function search(event) {
-    setKeyword(event.target.value);
-  }
-
-  function handleResponse(response) {
-    setResults(response.data);
-  }
-
+  
   return (
     <div className="Dictionary">
       <div className="Dictionary-search text-center">
@@ -41,6 +54,7 @@ export default function Dictionary() {
         </form>
       </div>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
